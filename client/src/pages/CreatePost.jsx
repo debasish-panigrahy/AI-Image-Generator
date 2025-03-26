398d28ef4ac6cafd89ca53f955681d79fe3e613e
debaasish-panigrahy <panigrahydebasish4@gmail.com>
2025-03-26 11:47:21 +0530

    Added Navbar and Searchbar

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import styled from 'styled-components';
+
+const Container = styled.div`
+    height:100%;
+    overflow-y:scroll;
+`;
 
 const CreatePost = () => {
   return (
-    <div>CreatePost</div>
+    <Container>CreatePost</Container>
   )
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import styled from 'styled-components';
+import SearchBar from '../components/SearchBar';
 
+const Container = styled.div`
+    height:100%;
+    overflow-y:scroll;
+    background:${({ theme}) => theme.bg};
+    padding:30px 30px;
+    padding-bottom:50px;
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    gap:20px;
+    @media(max-width: 768) {
+      padding:6px 10px;
+    }
+`;
+
+const HeadLine = styled.div`
+  font-size:34px;
+  font-weight:500;
+  color: ${({ theme }) => theme.text_primary};
+  display:flex;
+  align-items:center;
+  flex-direction:column;
+  @media (max-widt: 600) {
+    font-size:22px;
+  }
+`
+
+const Span = styled.div`
+  font-size:38px;
+  font-weight:800;
+  color: ${({ theme }) => theme.secondary};
+`
 const Home = () => {
   return (
-    <div>Home</div>
+    <Container>
+       <HeadLine>Explore popular posts in the community!
+       <Span>Generated with AI</Span>
+       </HeadLine>
+       <SearchBar />
+    </Container>
   )
 }
 
